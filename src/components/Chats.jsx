e5674052594a7b5c2d4ce1659e5843f57c04f06d
kyashwanthreddy01 <113547969+kyashwanthreddy01@users.noreply.github.com>
2023-07-14 23:46:16 +0530

    chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import user from '../static/images/user.png'
 
 const Chats = () => {
   return (
-    <div>
-      Chats
+    <div className='chats'>
+    
+      <div className='userChat'>
+        <img src={user} alt="user" />
+        <div className="userChatInfo">
+          <span>yash</span>
+          <p>Hello world</p>
+        </div>
+      </div>
+      
     </div>
+
   )
 }
 
