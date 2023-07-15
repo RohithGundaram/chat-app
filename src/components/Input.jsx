7086f2c46f1184f0e1a0b680d72bb14fb1052a88
Gundaram Raju Rohith <87968757+RohithGundaram@users.noreply.github.com>
2023-07-15 13:12:29 +0530

    styling done

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import attach from '../static/images/attach.png'
+import img from '../static/images/img.png'
 const Input = () => {
   return (
     <div className='input'>
-      <input type="text" />
+      <input type="text" placeholder='type something....'/>
+      <div className="send">
+        <img src={attach} alt="" />
+        <input type="file" style={{display:"none"}} id="file"/>
+        <label htmlFor="file">
+          <img src={img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   )
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import user from "../static/images/user.png"
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className="message">
+      <div className="messageInfo">
+        <img src={user} alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p> Hello </p>
+        <img src={user} alt="" />
+      </div>
     </div>
   )
 }
